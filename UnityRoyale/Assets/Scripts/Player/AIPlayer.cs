9ca539a341a1956c43282197ac16985083f08e76
jenniec1225 <142662931+jenniec1225@users.noreply.github.com>
2024-06-09 20:33:46 +0900

    AI Agent 학습
    NavMesh 만드는 방법
    - ai agent 사용
    - ray 사용

---
+++ UnityRoyale/Assets/Scripts/Player/AIPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityRoyale
+{
+    public class AIPlayer : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        // Start is called before the first frame update
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if(Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                }
+
+
+                // raycast check
+                Debug.DrawRay(ray.origin, ray.direction * 100, color: Color.red, duration: 2);
+            }
+        }
+    }
+}
